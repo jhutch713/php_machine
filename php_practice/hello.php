ad3dbec849c16461a0ad17f59e2a4bf355db6036
jhutch713 <142631513+jhutch713@users.noreply.github.com>
2025-01-10 00:29:19 +0000

    end of jan 9

---
+++ php_practice/hello.php
@@ -0,0 +1,9 @@
+<?php
+
+$myName = "Jensen";
+$myFriend = "Sara";
+echo "Hello, World! \n";
+echo "My name is $myName \n";
+echo "My friends name is $myFriend \n";
+echo 'Hello $myFriend \n';
+echo "Hello $myFriend" . "how are you?";
